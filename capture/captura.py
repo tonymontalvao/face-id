3e48620052a657aa5a09ed22fc53c509bccab06f
Anthony Montalvão <a.montalvao@obvious.com.br>
2024-07-12 15:29:52 -0300

    a

--- capture/captura.py
+++ capture/captura.py
@@ -2,8 +2,8 @@
 import cv2
 import numpy as np
 
-classFace = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-classOlhos = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+classFace = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classOlhos = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 camera = cv2.VideoCapture(0)
 amostra, numAmostras = 1, 25
@@ -16,9 +16,9 @@ while True:
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     facesDetectadas = classFace.detectMultiScale(imagemCinza,
-                                               scaleFactor=1.5,
-                                               minSize=(100,100))
-    
+                                                 scaleFactor=1.5,
+                                                 minSize=(100, 100))
+
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
         regiao = imagem[y:y + a, x:x + l]
@@ -28,14 +28,13 @@ while True:
         for (ox, oy, ol, oa) in olhosDetectados:
             cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
 
-
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 if np.average(imagemCinza) > 110:
-                    imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+                    imagemFace = cv2.resize(
+                        imagemCinza[y:y + a, x:x + l], (largura, altura))
                     cv2.imwrite(f'fotos/p.{id}.{amostra:02}.jpg', imagemFace)
                     print(f'Foto {amostra:02} capturada com sucesso')
                     amostra += 1
-
 
     cv2.imshow("Face", imagem)
     cv2.waitKey(1)
