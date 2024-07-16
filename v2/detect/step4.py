d98c08e84f2223273a5e68422c8484b24a8d659d
Anthony Montalvão <a.montalvao@obvious.com.br>
2024-07-16 01:06:45 -0300

    a

--- v2/detect/step4.py
+++ v2/detect/step4.py
@@ -37,8 +37,7 @@ def run(params):
         font = cv2.FONT_HERSHEY_DUPLEX
 
         if usb == 'True':
-            camera = cv2.VideoCapture(
-                0 + cv2.CAP_DSHOW, apiPreference=cv2.CAP_V4L2)
+            camera = cv2.VideoCapture(0, apiPreference=cv2.CAP_V4L2)
         else:
             camera = cv2.VideoCapture(0)
 
