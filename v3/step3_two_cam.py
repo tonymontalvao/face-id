171c9889da10daee6bfac5a04424f6490469d554
Anthony Montalvão <a.montalvao@obvious.com.br>
2024-08-09 17:45:59 -0300

    a

--- v3/step3_two_cam.py
+++ v3/step3_two_cam.py
@@ -62,15 +62,17 @@ def run(params):
         chrome_options.add_experimental_option(
             "excludeSwitches", ["enable-automation"])
 
-        if os.path.isfile('/usr/bin/chromedriver'):
-            service = Service('/usr/bin/chromedriver')
-        else:
-            try:
-                service = Service(ChromeDriverManager().install())
-            except:
-                service = Service(ChromeDriverManager(
-                    chrome_type=ChromeType.CHROMIUM).install())
+        # if os.path.isfile('/usr/bin/chromedriver'):
+        #     service = Service('/usr/bin/chromedriver')
+        # else:
+        #     try:
+        #         service = Service(ChromeDriverManager().install())
+        #     except:
+        #         service = Service(ChromeDriverManager(
+        #             chrome_type=ChromeType.CHROMIUM).install())
 
+        service = Service(ChromeDriverManager(
+            chrome_type=ChromeType.CHROMIUM).install())
         driver = webdriver.Chrome(options=chrome_options, service=service)
         driver.get(site)
         driver.fullscreen_window()
